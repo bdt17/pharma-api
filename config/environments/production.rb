29ba6ca6849cb6dba0cf5cd47ed5f48fd9241d20
bdt17 <brett.thomas29.97@gmail.com>
2025-12-14 21:11:09 -0700

    Production: disable AR encryption

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
   config.active_record.encryption.enabled = false
+  config.active_record.encryption.primary_key = nil
+  config.active_record.encryption.deterministic_key = nil
   config.force_ssl = false
+  config.cache_classes = true
+  config.eager_load = true
 end
