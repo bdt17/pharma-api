a4e3de4f4ca8a1dff9b21837c3f936e840c38ad2
bdt17 <brett.thomas29.97@gmail.com>
2025-12-14 21:10:01 -0700

    Fix AR encryption

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,4 @@
 Rails.application.configure do
   config.active_record.encryption.enabled = false
   config.force_ssl = false
-  config.cache_classes = true
-  config.eager_load = true
-  config.public_file_server.enabled = true
 end
