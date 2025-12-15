dedf76b26673ae12bc377c951b74cc10e77133f1
bdt17 <brett.thomas29.97@gmail.com>
2025-12-15 16:32:01 -0700

    STATIC JSON - NO CONTROLLERS

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  get "/api/v1/batches", to: "batches#index"
+  get "/api/v1/batches", to: proc do
+    [200, {'Content-Type' => 'application/json'}, [
+      {"id":1,"lot_number":"LOT123","temperature":2.5,"status":"Green"},
+      {"id":2,"lot_number":"LOT456","temperature":6.1,"status":"Alert"}
+    ]]
+  end
 end
