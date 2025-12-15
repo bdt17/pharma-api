eb90e5d33eb61f89602cb6307c2c4ba0680571f0
bdt17 <brett.thomas29.97@gmail.com>
2025-12-15 14:44:55 -0700

    Phase 8: remove ALL duplicates

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get "batches/index"
-      get "batches/show"
-      get "batches/create"
-      get "batches/update"
-      get "batches/destroy"
-      resources :shipments  # ← existing
-      resources :batches    # ← ADD THIS
+      resources :batches
+      resources :shipments
     end
   end
   # ... rest of routes
