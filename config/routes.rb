fdb83b8e964654e07fb8f37ed4e45eb5204b0aeb
bdt17 <brett.thomas29.97@gmail.com>
2025-12-18 21:13:16 -0700

    add api/v1/vehicles hardcoded 3 trucks

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :batches, only: [:index]
+      
+      # ADD THIS 👇
+      get :vehicles, to: proc {
+        [200, 
+         {'Content-Type' => 'application/json'}, 
+         [{id: 1, name: "F-150", lat: 33.44, lng: -112.07, status: "Phoenix → Tucson"},
+          {id: 2, name: "Transit", lat: 32.22, lng: -110.97, status: "Tucson → Hospital"},
+          {id: 3, name: "Box Truck", lat: 33.45, lng: -112.06, status: "Local delivery"}].to_json
+        ]
+      }
     end
   end
 end
